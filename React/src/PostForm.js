9ab6506de985bc070dd4a3064ab1ad6034fad305
mynyamray <59934771+mynyamray@users.noreply.github.com>
2023-04-30 22:41:32 -0700

    Add files via upload

---
+++ React/src/PostForm.js
@@ -0,0 +1,39 @@
+import React, {useState} from 'react';
+
+function PostForm() {
+    const [content, setContent] = useState('');
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const url = 'http://';
+        const data = {content};
+        fetch(url, {
+            method: 'POST',
+            header: {'Content-Type': 'application/json'},
+            body: JSON.stringify(data),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log('Post submitted: ', data);
+            })
+            .catch((error) => console.error('Error submitting', error));
+    };
+    return (
+        <form onSubmit={handleSubmit}>
+            <div
+                contentEditable
+                role="textbox"
+                inputMode="text"
+                style={{
+                border: '1px solid #ccc',
+                borderRadius: '4px',
+                minHeight: '100px',
+                padding: '8px',
+                }}
+                onInput={(e) => setContent(e.target.textContent)}
+                dangerouslySetInnerHTML={{ __html: content }}
+            />
+            <button type="submit">Vent</button>
+        </form>
+    )
+}
+export default PostForm;
